20bf1a2d5302a2d88add5e2f8e755a04f9b44027
stolekostadinovski <stole_kostadinovski@hotmail.com>
2024-10-12 00:34:23 +0200

    Made some changes

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 // <snippet_all>
 using Microsoft.EntityFrameworkCore;
 // var
+//commiting
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
